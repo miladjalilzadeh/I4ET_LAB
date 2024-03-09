0b3da53946d6ccd57ce45c81c7d7becff81da6ee
miladjalilzadeh <162204073+miladjalilzadeh@users.noreply.github.com>
2024-03-09 22:11:23 +0100

    Add files via upload

---
+++ Milad/milad_jalilzadeh.ino
@@ -0,0 +1,25 @@
+//milad jalilzadeh
+int num=0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT); 
+}
+void loop() 
+{
+  if (Serial.available() > 0) 
+  {
+    num = Serial.read();
+    //77=M
+    if(num == 77)
+    {
+      Serial.println(num);
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+    //68=D
+    if(num == 68)
+    {
+      Serial.println(num);
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+  }
+}
